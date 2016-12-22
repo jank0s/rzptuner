69c1499d02c2525f30b62880ab9888fccb40a5a6
Lovro <lovro@viberate.com>
2016-12-22 11:09:53 +0100

    Create start and stop method

--- app/src/main/java/rzp/rzptuner/Tuner.java
+++ app/src/main/java/rzp/rzptuner/Tuner.java
@@ -21,14 +21,30 @@ public class Tuner {
     private AudioRecord audioRecord;
     private boolean isRecording;
     private volatile Note currentNote;
-
+    private volatile Handler handler;
 
     public Tuner(){
         bufferSize = AudioRecord.getMinBufferSize(44100, AudioFormat.CHANNEL_IN_DEFAULT, AudioFormat.ENCODING_PCM_16BIT);
+        readSize = bufferSize/2;
         buffer = new float[readSize];
         intermediaryBuffer = new float[readSize];
         isRecording = false;
         audioRecord = new AudioRecord(MediaRecorder.AudioSource.DEFAULT, sampleRate, AudioFormat.CHANNEL_IN_DEFAULT, AudioFormat.ENCODING_PCM_16BIT, bufferSize);
         currentNote = new Note(Note.DEFAULT_FREQUENCY);
+        handler = new Handler(Looper.getMainLooper());
+    }
+
+    public void start(){
+        isRecording = true;
+        if(audioRecord!=null){
+            audioRecord.startRecording();
+        }
+    }
+
+    public void stop(){
+        isRecording = false;
+        if(audioRecord!=null){
+            audioRecord.stop();
+        }
     }
 }
